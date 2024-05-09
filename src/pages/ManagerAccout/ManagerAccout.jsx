eac855d9fb300a32c075fb28292ba61ffac119b1
Phi Hien <nguyenphihien1011@gmail.com>
2024-05-10 00:27:18 +0700

    Thêm user

--- src/pages/ManagerAccout/ManagerAccout.jsx
+++ src/pages/ManagerAccout/ManagerAccout.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch, faBuildingColumns, faCircleInfo } from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faBuildingColumns, faAddressBook,faTimes } from "@fortawesome/free-solid-svg-icons";
 import Sidebar from "../../components/Siderbar/Siderbar";
 import Pagination from "../../components/Pagination/Pagination";
 import { Link } from "react-router-dom";
@@ -10,9 +10,24 @@ const ManageUserAccount = () => {
     const [users, setUsers] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
     const [currentPage, setCurrentPage] = useState(1);
-
+    const [showAddAccountPage, setShowAddAccountPage] = useState(false);
+    const [newAccountData, setNewAccountData] = useState({
+        email: "",
+        password: "",
+        role: "",
+    });
+    const [roles, setRoles] = useState([]);
     useEffect(() => {
-        axios.get(`https://c2se-14-sts-api.onrender.com/api/search?searchTerm=${searchTerm}&page=${currentPage}`) // Truyền currentPage vào URL
+        axios.get("https://c2se-14-sts-api.onrender.com/api/roleName")
+            .then(response => {
+                setRoles(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching roles:', error);
+            });
+    }, []);
+    useEffect(() => {
+        axios.get(`https://c2se-14-sts-api.onrender.com/api/search?searchTerm=${searchTerm}&page=${currentPage}`)
             .then(response => {
                 const userData = response.data.map(user => {
                     if (user.role) {
@@ -38,13 +53,13 @@ const ManageUserAccount = () => {
             .catch(error => {
                 console.error('Error fetching data:', error);
             });
-    }, [searchTerm, currentPage]); // Thêm currentPage vào dependency array
+    }, [searchTerm, currentPage]);
+
     function formatCurrency(value) {
-        // Chuyển đổi giá trị thành số và làm tròn đến hàng trăm gần nhất
         const roundedValue = Math.round(parseFloat(value) * 100) / 100;
-        // Sử dụng hàm toLocaleString() để format theo ngôn ngữ địa phương và thêm dấu phẩy
         return roundedValue.toLocaleString('vi-VN', { style: 'currency', currency: 'VND', minimumFractionDigits: 0, maximumFractionDigits: 0 });
     }
+
     const handleSearch = (event) => {
         setSearchTerm(event.target.value);
         setCurrentPage(1);
@@ -52,9 +67,9 @@ const ManageUserAccount = () => {
 
     const handlePageChange = (direction) => {
         if (direction === 'next') {
-            setCurrentPage(prevPage => prevPage + 1); // Tăng trang hiện tại lên 1 khi chuyển qua trang tiếp theo
+            setCurrentPage(prevPage => prevPage + 1);
         } else if (direction === 'prev' && currentPage > 1) {
-            setCurrentPage(prevPage => prevPage - 1); // Giảm trang hiện tại đi 1 khi chuyển về trang trước đó (phải đảm bảo currentPage > 1)
+            setCurrentPage(prevPage => prevPage - 1);
         }
     };
 
@@ -71,6 +86,33 @@ const ManageUserAccount = () => {
         console.log(userId);
     };
 
+    const handleAddAccountClick = () => {
+        setShowAddAccountPage(true);
+    };
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setNewAccountData(prevData => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const handleSaveNewAccount = async () => {
+        try {
+            const response = await axios.post("https://c2se-14-sts-api.onrender.com/api/users", newAccountData);
+            console.log("Tài khoản đã được tạo thành công:", response.data);
+            setShowAddAccountPage(false);
+            setNewAccountData({
+                email: "",
+                password: "",
+                role: "",
+            });
+        } catch (error) {
+            console.error("Lỗi khi tạo tài khoản mới:", error);
+        }
+    };
+
     return (
         <div className="bg-[#F3F7FA] w-full h-full p-8">
             <div className="grid grid-cols-12 gap-10">
@@ -80,32 +122,91 @@ const ManageUserAccount = () => {
                     </div>
                 </div>
                 <div className=" ml-24 col-span-10">
+                    {showAddAccountPage ? (
+                        <div className="bg-white p-8 rounded-lg w-[700px] shadow-lg relative">
+                            <h2 className="text-lg font-semibold mb-4">Thêm tài khoản mới</h2>
+                            <div className="mb-4">
+                                <label className="block text-sm font-medium text-gray-700">Email:</label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    value={newAccountData.email}
+                                    onChange={handleInputChange}
+                                    className="mt-1 p-2 border rounded w-full"
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-sm font-medium text-gray-700">Mật khẩu:</label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    value={newAccountData.password}
+                                    onChange={handleInputChange}
+                                    className="mt-1 p-2 border rounded w-full"
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-sm font-medium text-gray-700">Vai trò:</label>
+                                <select
+                                    name="role"
+                                    value={newAccountData.role}
+                                    onChange={handleInputChange}
+                                    className="mt-1 p-2 border rounded w-full"
+                                >
+                                    <option value="">Chọn vai trò</option>
+                                    {roles.map(role => (
+                                        <option key={role.id} value={role.id}>{role.name}</option>
+                                    ))}
+                                </select>
+                            </div>
+                            <button
+                                onClick={handleSaveNewAccount}
+                                className="py-2 px-4 bg-green-500 hover:bg-green-700 text-white rounded mr-2"
+                            >
+                                Lưu
+                            </button>
+                            <button
+                                onClick={() => setShowAddAccountPage(false)}
+                                className="absolute top-4 right-4 text-gray-600 hover:text-gray-800 flex items-center"
+                            >
+                                <FontAwesomeIcon icon={faTimes} className="mr-2" />
+                                Hủy
+                            </button>
+                        </div>
+                    ) :(
+
                     <div className="bg-[#ffff] border border-white p-2 rounded-lg">
-                        <div className="mx-auto border border-white p-2">
-                            <div className="App p-2 flex items-center justify-between">
-                                <div style={{ textAlign: "left" }}>
-                                    <h1>Danh sách người dùng</h1>
-                                    <i className="text-green-700 py-4" style={{ width: "20%" }}>Active Members</i>
-                                </div>
-                                <div className="search-box flex bg-[#F9FAFB] w-[700px] h-[45px] border border-black rounded-s-2xl px-4">
-                                    <input
-                                        className="search-input w-9/12 font-bold outline-none bg-transparent pl-2"
-                                        type="text"
-                                        placeholder="Tìm kiếm theo họ và tên hoặc tên người dùng..."
-                                        value={searchTerm}
-                                        onChange={handleSearch}
-                                    />
-                                </div>
-                                <div className="bg-[#212143] border border-black rounded-e-2xl">
-                                    <button className="search-button p-1 w-[105px] h-[45px] text-white">
-                                        <FontAwesomeIcon icon={faSearch} /> Tìm kiếm
-                                    </button>
+                            <div className="mx-auto border border-white p-2">
+                                <div className="App p-2 flex items-center justify-between">
+                                    <div style={{ textAlign: "left" }}>
+                                        <h1>Danh sách người dùng</h1>
+                                        <i className="text-green-700 py-4" style={{ width: "20%" }}>Active Members</i>
+                                    </div>
+                                    <div className="search-box flex bg-[#F9FAFB] w-[500px] h-[45px] border border-black rounded-s-2xl px-4">
+                                        <input
+                                            className="search-input w-9/12 font-bold outline-none bg-transparent pl-2"
+                                            type="text"
+                                            placeholder="Tìm kiếm theo họ và tên hoặc tên người dùng..."
+                                            value={searchTerm}
+                                            onChange={handleSearch}
+                                        />
+                                    </div>
+                                    <div className="bg-[#212143] border border-black rounded-e-2xl">
+                                        <button className="search-button text-lg p-1 w-[120px] h-[45px] text-white ml-2 ">
+                                            <FontAwesomeIcon icon={faSearch} /> Tìm kiếm
+                                        </button>
+                                    </div>
+                                    <div className="bg-[#212143] border mx-2 border-black rounded ">
+                                        <button className="search-button p-1 text-lg w-[180px] h-[45px] text-white"
+                                                onClick={handleAddAccountClick}>
+                                            <FontAwesomeIcon icon={faAddressBook} className="mx-1" /> Thêm tài khoản
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="w-auto mx-4 h-full text-left bg-white rounded-lg shadow-lg py-10">
-                            <table className="min-w-full border-collapse w-full">
-                                <thead>
+                            <div className="w-auto mx-4 h-full text-left bg-white rounded-lg shadow-lg py-10">
+                                <table className="min-w-full border-collapse w-full">
+                                    <thead>
                                     <tr className="text-gray-500">
                                         <th className="py-2 px-3 border-t text-black border-gray-300 bg-white">STT</th>
                                         <th className="py-2 px-3 border-t text-black border-gray-300 bg-white">Họ và tên</th>
@@ -114,8 +215,8 @@ const ManageUserAccount = () => {
                                         <th className="py-2 px-3 border-t text-black border-gray-300 bg-white">Số dư</th>
                                         <th className="py-2 px-3 border-t text-black border-gray-300 bg-white">Tổng giao dịch</th>
                                     </tr>
-                                </thead>
-                                <tbody>
+                                    </thead>
+                                    <tbody>
                                     {filteredUsers.map((user, index) => (
                                         <tr key={index} className="text-gray-500">
                                             <td className="py-2 px-3 border-t text-black border-gray-300 bg-white">{index + 1}</td>
@@ -137,14 +238,14 @@ const ManageUserAccount = () => {
                                             </td>
                                         </tr>
                                     ))}
-                                </tbody>
-                            </table>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div className="border border-white py-8">
+                                <Pagination currentPage={currentPage} onPageChange={handlePageChange} />
+                            </div>
                         </div>
-                        <div className="border border-white py-8">
-                            {/* Truyền onPageChange vào PaginationAdmin */}
-                            <Pagination currentPage={currentPage} onPageChange={handlePageChange} />
-                        </div>
-                    </div>
+                    )}
                 </div>
             </div>
         </div>
